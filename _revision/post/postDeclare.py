a5a72ed66f36454aec41fe74c6158572cd720b18
abdlazznp <abdul.aziz@nicepay.co.id>
2023-06-23 09:33:54 +0700

    Adding new file refactoring py-snap

--- _revision/post/postDeclare.py
+++ _revision/post/postDeclare.py
@@ -1,3 +1,4 @@
+from builder.builderVirtualAccount import virtualAccountBuilder, variableVirtualAccount
 from util.utilLogging import LoggerPrint
 from postVirtualAccountReq import postVirtualAccountReq
 from config.configConstants import Constants
@@ -30,13 +31,14 @@ class PostDeclare:
             headersMap["X-CLIENT-KEY"] = clientKey
             headersMap["X-SIGNATURE"] = signatureAccessToken
         else:
-            hashData = Signature.sha256EncodeHex(data)
+            hashData = Signature.sha256EncodeHex(json.dumps(data))
             signature = Signature.getSignature(accessToken, hashData, url, timestamp, secretKey)
             headersMap["Authorization"] = f"Bearer {accessToken}"
             headersMap["X-TIMESTAMP"] = timestamp
-            headersMap["X-CLIENT-KEY"] = clientKey
+            # headersMap["X-CLIENT-KEY"] = clientKey
             headersMap["X-SIGNATURE"] = signature
             headersMap["X-EXTERNAL-ID"] = externalId
+            headersMap["X-PARTNER-ID"] = clientKey
             headersMap["CHANNEL-ID"] = clientKey + "01"
 
         # log.logInfo("Request Header : " + json.dumps(headersMap))
@@ -47,24 +49,38 @@ class PostDeclare:
 
         # CALL TOKEN
         log.logInfo("[GENERATE ACCESS TOKEN]")
-
+        headers = PostDeclare.getHeaders(grantType, accessToken, data, "/v1.0/access-token/b2b")
         response = postVirtualAccountReq.getToken(Constants.getSandboxBaseUrl(),
-                                                  PostDeclare.getHeaders(grantType,
-                                                                         accessToken,
-                                                                         data,
-                                                                         "/v1.0/access-token/b2b"),
-                                                  {
-                                                      "grantType": "client_credentials",
-                                                      "additionalInfo": ""
-                                                  })
+                                                  headers,
+                                                  createToken.toString()
+                                                  )
         a = json.dumps(response[0])
+        data = json.loads(a)
+        accessToken = data["accessToken"]
+        # jsonData = response[0]
+        # data = json.loads(jsonData)
+        # access_token = data["accessToken"]
         # getAccessToken = json.loads(response[0])
         # AccessToken = getAccessToken["accessToken"]
         log.logInfo("genAccessToken Endpoint : " + json.dumps(response[1]))
         log.logInfo("genAccessToken FullUrl : " + Constants.getSandboxBaseUrl() + response[1])
-        log.logInfo("genAccessToken Headers : " + json.dumps(createToken.toString()))
+        log.logInfo("genAccessToken Headers : " + json.dumps(headers))
+        log.logInfo("genAccessToken Body : " + json.dumps(createToken.toString()))
         log.logInfo("genAccessToken Response : " + a)
-        # log.logInfo("AccessToken : " + accessToken )
+
+        # CALL CREATE VA
+        log.logInfo("[CREATE VIRTUAL ACCOUNT]")
+        headers2 = PostDeclare.getHeaders(None, accessToken, createVA.toString(),
+                                         "/api/v1.0/transfer-va/create-va")
+        response2 = postVirtualAccountReq.createVA(Constants.getSandboxBaseUrl(),
+                                                   headers2,
+                                                   createVA.toString())
+        b = json.dumps(response2[0])
+        log.logInfo("createVA Endpoint : " + json.dumps(response2[1]))
+        log.logInfo("createVA FullUrl : " + Constants.getSandboxBaseUrl() + response2[1])
+        log.logInfo("createVA Headers : " + json.dumps(headers2))
+        log.logInfo("createVA Body : " + json.dumps(createVA.toString()))
+        log.logInfo("createVA Response : " + b)
 
         # StringURL = Constants.getSandboxBaseUrl()
         # a = postVirtualAccountReq.getToken(
@@ -92,6 +108,47 @@ createToken = (
     .setAdditionalInfo("")
     .build()
 
+)
+
+
+class testVirtualAccount(virtualAccountBuilder):
+    def build(self):
+        return variableVirtualAccount(
+            self.partnerServiceId,
+            self.customerNo,
+            self.virtualAccountNo,
+            self.virtualAccountName,
+            self.trxId,
+            self.totalAmount,
+            self.additionalInfo
+        )
+
+
+totalAmount = {"value": "10000.00",
+               "currency": "IDR"
+               }
+
+additionalInfo = {"bankCd": "CENA",
+                  "goodsNm": "Merchant Goods 1",
+                  "dbProcessUrl": "https://webhook.site/e15ef201-98a9-428c-85d4-a0c6458939c3",
+                  "vacctValidDt": "",
+                  "vacctValidTm": "",
+                  "msId": "",
+                  "msFee": "",
+                  "msFeeType": "",
+                  "mbFee": "",
+                  "mbFeeType": ""
+                  }
+createVA = (
+    testVirtualAccount()
+    .setPartnerServiceId("IONPAYTEST")
+    .setCustomerNo("")
+    .setVirtualAccountNo("")
+    .setVirtualAccountName("Hantu Kesorean")
+    .setTrxId("IONPAYTEST1348844834384834")
+    .setTotalAmount(totalAmount)
+    .setAdditionalInfo(additionalInfo)
+    .build()
 )
 
 PostDeclare.serviceSNAP("client_credentials", "aaaaa", {})
--- _revision/util/utilSignature.py
+++ _revision/util/utilSignature.py
@@ -8,14 +8,11 @@ import hashlib
 import hmac
 
 class Signature :
-    print = LoggerPrint()
+    log = LoggerPrint()
 
     @staticmethod
     def signSHA256RSA(stringToSign, privateKey):
         try:
-            # b1 = RSA.importKey(privateKey)
-            # privateKeySigner = PKCS1_v1_5.PKCS115_SigScheme(b1)
-            # Signature = privateKeySigner.sign(SHA256.new(stringToSign.encode()))
             b1 = base64.b64decode(privateKey)
             key = RSA.importKey(b1)
             signer = PKCS1_v1_5.new(key)
@@ -25,16 +22,16 @@ class Signature :
             hexSignature = base64.b64encode(signature).decode('utf-8')
             return hexSignature
         except Exception as e:
-            print("Error Generate Signature:", e)
+            Signature.log.logError("Error Generate Signature:", e)
             return ''
 
     @staticmethod
     def getSignature(accessToken, requestBody, url, timestamp, staticKey):
         data = f"POST:{url}:{accessToken}:{requestBody}:{timestamp}"
-        Signature.print.logInfo("String Data Sign: " + data)
+        Signature.log.logInfo("String Data Sign: " + data)
         try:
             sign = Signature.hmacSHA512EncodeBase64(staticKey, data)
-            Signature.print.logInfo("This Signature: " + sign)
+            Signature.log.logInfo("This Signature: " + sign)
             return sign
         except Exception as e:
             return ''
